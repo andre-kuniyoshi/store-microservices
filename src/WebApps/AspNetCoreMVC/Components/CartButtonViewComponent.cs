8b52a77c7fd8cb9642c0b63d745f69e6798ddfd4
Andre Kuniyoshi <yoshiniks@gmail.com>
2023-09-29 16:35:44 -0300

    adding CartButtonViewComponent in MVC

---
+++ src/WebApps/AspNetCoreMVC/Components/CartButtonViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreMVC.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVC.Components
+{
+    public class CartButtonViewComponent : ViewComponent
+    {
+        private readonly IBasketService _basketService;
+
+        public CartButtonViewComponent(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cartCount = await _basketService.GetBasketItemsCount();
+
+            return View(cartCount);
+        }
+    }
+}
